2e257c2f10afd51308ccc9b4aa7e25477095c24f
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2021-11-19 11:25:04 +0100

    update controller with coms to explain

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VideoRepository; //j'inclus le repository Video pour faire les requetes SQL
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/main", name="main")
+     * Je mets la variable $videoRepository en param pour pouvoir faire des requetes sur les videos
      */
-    public function index(): Response
+    public function index(VideoRepository $videoRepository): Response
     {
+        $all_videos = $videoRepository->findAll();//je stocke toutes mes videos dans une variable
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'all_videos' => $all_videos //avant la fleche --> nom de la variable twig, apres --> nom variable php
         ]);
     }
 }
