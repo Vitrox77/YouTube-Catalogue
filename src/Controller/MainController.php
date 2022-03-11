7294da4e120d1f8a13d9cfbb0cc1e59418e73b27
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2022-03-11 11:37:15 +0100

    to help

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,12 +15,13 @@ class MainController extends AbstractController
      */
     public function index(CategoriesRepository $categoryRepository): Response
     {
+        // https://stackoverflow.com/questions/31878323/add-data-when-running-symfony-migrations
         $array_age = ['20','10', '12'];
         $categories = $categoryRepository->findAll();//je stocke toutes mes videos dans une variable
         return $this->render('menu/index.html.twig', [
             'categories' => $categories,
             'age' => $array_age,
-            'controller_name' => 'MainController',
+            'controller_name' => 'MenuController',
         ]);
     }
 }
