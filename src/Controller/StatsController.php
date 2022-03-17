ab38109c8a6d2ab35a3c999dda7385dac684aeff
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2022-03-17 18:44:03 +0100

    add route stats

---
+++ src/Controller/StatsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StatsController extends AbstractController
+{
+    /**
+     * @Route("/stats", name="stats_globale")
+     */
+    public function index(): Response
+    {
+        return $this->render('stats/statsGlobale.html.twig', [
+            'controller_name' => 'StatsController',
+        ]);
+    }
+}
