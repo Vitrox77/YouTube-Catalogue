7ea8fa76a46ef03692faeaf3f1a8b251f294b265
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2022-03-18 10:58:47 +0100

    add forms

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\ImportCSVType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategoriesRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends AbstractController
 {
@@ -13,15 +15,27 @@ class MainController extends AbstractController
      * @Route("/main", name="app_main")
      * Je mets la variable $categoryRepository en param pour pouvoir faire des requetes sur les videos
      */
-    public function index(CategoriesRepository $categoryRepository): Response
+    public function index(CategoriesRepository $categoryRepository, Request $request): Response
     {
         // https://stackoverflow.com/questions/31878323/add-data-when-running-symfony-migrations
         $array_age = ['20','10', '12'];
         $categories = $categoryRepository->findAll();//je stocke toutes mes videos dans une variable
+        
+
+
+        $form = $this->createForm(ImportCSVType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //todo
+            return $this->redirectToRoute('app_main');
+        }
+
         return $this->render('menu/index.html.twig', [
             'categories' => $categories,
             'age' => $array_age,
             'controller_name' => 'MenuController',
+            'CSVform' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/ImportCSVType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImportCSVType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('CSV_file', FileType::class, [
+            'label' => 'Brochure (CSV file)',
+
+            // unmapped means that this field is not associated to any entity property
+            'mapped' => false,
+
+            // make it optional so you don't have to re-upload the PDF file
+            // every time you edit the Product details
+            'required' => true,
+
+            // unmapped fields can't define their validation using annotations
+            // in the associated entity, so you can use the PHP constraint classes
+            'constraints' => [
+                new File([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'text/csv',
+                        'application/csv',
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid CSV document',
+                ])
+            ],
+        ])
+        ->add('button', ButtonType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
