2d4e80a87f039dab9723bd5f4a5a75e8354ba20b
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2022-03-23 15:15:11 +0100

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -32,75 +32,96 @@ class MainController extends AbstractController
 
         $URLform->handleRequest($request);
         $CSVform->handleRequest($request);
+
+        /* ----------------------------------------------------------------------------------------------------------------------------- */
+        /* ---------------------------------------- ZONE DU FORMULAIRE D'UN LIEN UNIQUE ------------------------------------------------ */
+        /* ----------------------------------------------------------------------------------------------------------------------------- */
         
         if ($URLform->isSubmitted() && $URLform->isValid()) {
-            //todo appeler l'api avec l'url donné
-            $data = $URLform->getData();
-            $videoId = substr($data['unique_link'], -11);
-            //toDo asynchrone
-            $json = $callApiService->getVideoJson($videoId);
 
-            //toDo enregistrer en base les infos
-            $newVideo = new Video();
-            
-            /* ************************ GESTION DE LA CATEGORIE ****************************** */
-            $category = $json['result']['categories'][0];
-            $categoryRepository = $doctrine->getRepository(Categories::class);
-            $searchedCategory = $categoryRepository->findOneOrNullByName($category);
-            //si la catégorie n'existe pas, on la créée
-            if($searchedCategory == NULL){
-                $newCategory = new Categories();
-                $newCategory->setName($category);
-                $entityManager->persist($newCategory);
-                $entityManager->flush();
-                $finalCategory = $categoryRepository->findOneBy([
-                    'name' => $json['result']['categories'][0]
-                ]);
-                $newVideo->setUploader($finalCategory);
+            $data = $URLform->getData();//je recup le lien
+            $videoId = substr($data['unique_link'], -11);//je le coupe pour prendre que l'id
+
+            //on vérifie avant de faire l'appel à l'api si la vidéo existe déjà
+            $videoRepository = $doctrine->getRepository(Video::class);
+            $searchedVideo = $videoRepository->findOneOrNullById($videoId);
+            if($searchedVideo != NULL){
+                //si la video existe
+                $videoTitle = 'La vidéo existe déjà dans la base de données et ne sera donc pas téléchargée';
             }else{
-                $newVideo->setCategory($searchedCategory);
-            }
-            /* ******************************************************************************** */
+                //toDo asynchrone
+                $json = $callApiService->getVideoJson($videoId);
 
-            /* ************************* GESTION DE L'UPLOADER ******************************** */
-            $uploader_id = $json['result']['uploader_id'];
-            $uploaderRepository = $doctrine->getRepository(Uploader::class);
-            $searchedUploader = $uploaderRepository->findOneOrNullByYtId($uploader_id);
-            //si l'uploader n'existe pas, on le crée
-            if($searchedUploader == NULL){
-                $newUploader = new Uploader();
-                $newUploader->setChannelId($uploader_id);
-                $newUploader->setName($json['result']['channel']);
-                $entityManager->persist($newUploader);
+                //toDo enregistrer en base les infos
+                $newVideo = new Video();
+                $videoTitle = $json['result']['title'];
+                
+                /* ************************ GESTION DE LA CATEGORIE ****************************** */
+                $category = $json['result']['categories'][0];
+                $categoryRepository = $doctrine->getRepository(Categories::class);
+                $searchedCategory = $categoryRepository->findOneOrNullByName($category);
+                //si la catégorie n'existe pas, on la créée
+                if($searchedCategory == NULL){
+                    $newCategory = new Categories();
+                    $newCategory->setName($category);
+                    $entityManager->persist($newCategory);
+                    $entityManager->flush();
+                    $finalCategory = $categoryRepository->findOneBy([
+                        'name' => $json['result']['categories'][0]
+                    ]);
+                    $newVideo->setUploader($finalCategory);
+                }else{
+                    $newVideo->setCategory($searchedCategory);
+                }
+                /* ******************************************************************************** */
+
+                /* ************************* GESTION DE L'UPLOADER ******************************** */
+                $uploader_id = $json['result']['uploader_id'];
+                $uploaderRepository = $doctrine->getRepository(Uploader::class);
+                $searchedUploader = $uploaderRepository->findOneOrNullByYtId($uploader_id);
+                //si l'uploader n'existe pas, on le crée
+                if($searchedUploader == NULL){
+                    $newUploader = new Uploader();
+                    $newUploader->setChannelId($uploader_id);
+                    $newUploader->setName($json['result']['channel']);
+                    $entityManager->persist($newUploader);
+                    $entityManager->flush();
+                    $finalUploader = $uploaderRepository->findOneBy([
+                        'name' => $json['result']['channel'],
+                        'channel_id' => $uploader_id
+                    ]);
+                    $newVideo->setUploader($finalUploader);
+                }else{
+                    $newVideo->setUploader($searchedUploader);
+                }
+                /* ******************************************************************************** */
+
+                $newVideo->setTitle($json['result']['title']);//title
+                $newVideo->setUrl($json['result']['id']);//id de la video
+                $newVideo->setThumbnail($json['result']['thumbnail']);//url de la thumbnail (todo save dans le serv et mettre le chemin)
+                $newVideo->setUploadDate(new DateTime($json['result']['upload_date']));//upload date
+                $newVideo->setDescription($json['result']['description']);//description
+                $newVideo->setDuration($json['result']['duration']);//durée en sec
+                $newVideo->setDownloadDate(new DateTime('now'));//date de dl pour nous
+
+                //j'envoi en bdd
+                $entityManager->persist($newVideo);
                 $entityManager->flush();
-                $finalUploader = $uploaderRepository->findOneBy([
-                    'name' => $json['result']['channel'],
-                    'channel_id' => $uploader_id
-                ]);
-                $newVideo->setUploader($finalUploader);
-            }else{
-                $newVideo->setUploader($searchedUploader);
-            }
-            /* ******************************************************************************** */
 
-            $newVideo->setTitle($json['result']['title']);//title
-            $newVideo->setUrl($json['result']['id']);//id de la video
-            $newVideo->setThumbnail($json['result']['thumbnail']);//url de la thumbnail (todo save dans le serv et mettre le chemin)
-            $newVideo->setUploadDate(new DateTime($json['result']['upload_date']));//upload date
-            $newVideo->setDescription($json['result']['description']);
-            $newVideo->setDuration($json['result']['duration']);
-            $newVideo->setDownloadDate(new DateTime('now'));
-            //j'envoi en bdd
-            $entityManager->persist($newVideo);
-            $entityManager->flush();
+            }//end else video existe pas
+                
 
             return $this->render('menu/index.html.twig', [
-                'controller_name' => 'MenuController',
                 'CSVform' => $CSVform->createView(),
                 'URLform' => $URLform->createView(),
-                'videoData' => $json
+                'videoTitle' => $videoTitle
             ]);
         }
+
+
+        /* ----------------------------------------------------------------------------------------------------------------------------- */
+        /* ------------------------------------------ ZONE DU FORMULAIRE DU FICHIER CSV ------------------------------------------------ */
+        /* ----------------------------------------------------------------------------------------------------------------------------- */
 
         if ($CSVform->isSubmitted() && $CSVform->isValid()) {
             $file = $CSVform->get('CSV_file')->getData();
@@ -125,7 +146,6 @@ class MainController extends AbstractController
         }
 
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
             'CSVform' => $CSVform->createView(),
             'URLform' => $URLform->createView()
         ]);
