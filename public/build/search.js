b2c99efd71ac3c5f5df4b8a88789cbed16043227
diye1210 <74767822+diye1210@users.noreply.github.com>
2022-03-24 21:08:27 +0100

    search_page

---
+++ public/build/search.js
@@ -0,0 +1,46 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([
+    ["search"], {
+
+        /***/
+        "./assets/js/search.js":
+        /*!***************************!*\
+          !*** ./assets/js/search.js ***!
+          \***************************/
+        /***/
+            ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+            __webpack_require__.r(__webpack_exports__);
+            /* harmony import */
+            var _styles_search_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__( /*! ../styles/search.css */ "./assets/styles/search.css");
+
+
+            /***/
+        }),
+
+        /***/
+        "./assets/styles/search.css":
+        /*!********************************!*\
+          !*** ./assets/styles/search.css ***!
+          \********************************/
+        /***/
+            ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+            __webpack_require__.r(__webpack_exports__);
+            // extracted by mini-css-extract-plugin
+
+
+            /***/
+        })
+
+    },
+    /******/
+    __webpack_require__ => { // webpackRuntimeModules
+        /******/
+        var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+            /******/
+            var __webpack_exports__ = (__webpack_exec__("./assets/js/search.js"));
+        /******/
+    }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVudS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FDQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvanMvbWVudS5qcyIsIndlYnBhY2s6Ly8vLi9hc3NldHMvc3R5bGVzL21lbnUuY3NzP2Y3NDIiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICcuLi9zdHlsZXMvbWVudS5jc3MnOyIsIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpblxuZXhwb3J0IHt9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
