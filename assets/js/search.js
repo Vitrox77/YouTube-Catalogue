0e6d8e9af920c9c98ac77c75dbe9ab49c1618254
Alexandre JOUDIOUX <alexandre.joudioux@edu.esiee.fr>
2022-05-16 10:43:38 +0200

    Add on click event to tag on modal + jquery working in modal

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,13 @@
 import '../styles/search.css';
+const $ = require('jquery');
+
+
+$(document).ready(function() {
+$(document).on("click", ".tag", function() {
+  
+    alert('clicked');
+    $(".tag").on('click', function() {
+        //(... rest of your JS code)
+        alert('clicked');
+    });
+});});
