6bfd233b991ace2b9f48a510e4323a26c4085de7
Alexandre <alexandre.joudioux@edu.esiee.fr>
2022-05-24 09:27:17 +0200

    Working on ajax call for tags

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,12 +2,40 @@ import '../styles/search.css';
 const $ = require('jquery');
 
 
+
 $(document).ready(function() {
-$(document).on("click", ".tag", function() {
+    $('#search-tag').each(function() {
+        var elem = $(this);
+        // Save current value of element
+        elem.data('oldVal', elem.val());
+     
+        // Look for changes in the value
+        elem.bind("keyup input paste", function(event){
+
+            if (elem.data('oldVal') != elem.val()) {
+
+                // Updated stored value
+                elem.data('oldVal', elem.val());
+
+                // Do AJAX call to get the tag corresponding to the search
+                $.ajax({
+                    url: '/search',
+                    type: 'POST',
+                    data: {
+                        'search': elem.val()
+                    },
+                    success: function(data) {
+                        $('#search-results').html(data);
+                    }
+                });
+            }
+        });
+    });
+    
+    $(document).on("click", ".tag", function() {
   
     alert('clicked');
-    $(".tag").on('click', function() {
-        //(... rest of your JS code)
-        alert('clicked');
+    
     });
-});});
+});
+
