0c974accb6fd8d15cd2434122a347e09a8b537e7
diye1210 <74767822+diye1210@users.noreply.github.com>
2022-06-02 18:07:46 +0200

    erreur modal

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,2 +1,16 @@
 import '../styles/search.css';
 const $ = require('jquery');
+
+// var myModal = document.getElementById('exampleModal')
+
+// $(document).ready(function() {
+//     alert();
+//     myModal.addEventListener('shown.bs.modal', function() {
+//         console.log("modal");
+
+//     })
+// });
+
+$(document).ready(function() {
+    alert("video.js");
+});
