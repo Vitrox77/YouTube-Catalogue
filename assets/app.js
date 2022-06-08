3346830b8e9275b478e1ef5583375f9747123cc1
Alexandre <alexandre.joudioux@edu.esiee.fr>
2022-06-08 11:37:41 +0200

    Fixing upload CSV button

--- assets/app.js
+++ assets/app.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
 
         // Bouton de reset de l'input file
         import_scv_reset.removeClass('d-none');
-        import_scv_reset.html('<i class="bi bi-x-circle"></i>');
+        import_scv_reset.html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>');
 
         //Changement du texte affiché sur la fenêtre
         before_submit_div.addClass('d-none');
